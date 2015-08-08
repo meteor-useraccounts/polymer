9ff197cbd245de9e385bf3f7bc42e23612d6dc60
Luca Mussi <ing.luca.mussi@gmail.com>
2015-08-08 15:28:10 +0200

    removed less dependency

--- package.js
+++ package.js
@@ -8,7 +8,6 @@ Package.on_use(function(api, where) {
   api.versionsFrom('METEOR@1.0');
 
   api.use([
-    'less',
     'templating',
   ], 'client');
 
@@ -58,7 +57,7 @@ Package.on_use(function(api, where) {
     'lib/at_title.html',
     'lib/at_title.js',
     'lib/full_page_at_form.html',
-    'lib/at_polymer.less'
+    'lib/at_polymer.css'
   ], ['client']);
 });
 
